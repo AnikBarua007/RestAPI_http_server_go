fcada531133a90da89f5a7e6364270eb40f579af
Anik Barua <anik.compsc@gmail.com>
2026-02-03 21:15:51 +0600

    adding the middlewaermatrics that wraps the handler

--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	apicfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	mux := http.NewServeMux()
 	//mux.Handle("/assets/", http.FileServer(http.Dir(".")))
-	mux.Handle("/app/", http.StripPrefix("/app"+
-		"", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", apicfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/matrics", apicfg.middlewareMetricsInc())
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -25,4 +33,11 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
