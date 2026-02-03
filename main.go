6460ae67e023af8470bd396b371e8c9b8fa7914a
Anik Barua <anik.compsc@gmail.com>
2026-02-03 15:43:19 +0600

    custom handler,here handler rediness is another handler

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/assets/", http.FileServer(http.Dir(".")))
+	//mux.Handle("/assets/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app"+
+		"", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
